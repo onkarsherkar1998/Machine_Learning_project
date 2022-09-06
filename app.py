478271c9d147f0e55f0fa45d24a78b254ef7946e
onkar sherkar <onkarsherkar1998@gmail.com>
2022-09-06 18:19:54 +0530

    a

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route("/",methods=['POST'])
+
+@app.route("/",methods=['GET','POST'])
 def index():
-    return"Starting machine learning project"
+    return "CI CD pipeline has been established."
 
-if(__name__)=="__main__":
+
+if __name__=="__main__":
     app.run(debug=True)
-        
