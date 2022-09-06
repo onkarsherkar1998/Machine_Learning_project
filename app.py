c0e0739a9d89a10acdfa66b34aafa740e2e76059
onkar sherkar <onkarsherkar1998@gmail.com>
2022-09-06 20:42:38 +0530

    deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "CI CD pipeline has been established."
+    return "starting machine learning project."
 
 
 if __name__=="__main__":
