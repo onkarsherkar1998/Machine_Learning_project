28363c399e6690a56787eab5efc6754c7dcfd6d1
onkar sherkar <onkarsherkar1998@gmail.com>
2022-09-09 11:54:23 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 def get_requirements_list()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .\n")
+        
+
 #declaring variables for setup functions
 PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
+VERSION = "0.0.3"
 AUTHOR = "Onkar Sherkar"
 DESCRIPTION = "This is 1st machine learning project"
-PACKAGES = ["housing"]
+
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
 
@@ -18,7 +20,7 @@ setup(
     version = VERSION,
     author = AUTHOR,
     description = DESCRIPTION,
-    packages = PACKAGES,
+    packages = find_packages(),
     install_requires = get_requirements_list()
 )
 
